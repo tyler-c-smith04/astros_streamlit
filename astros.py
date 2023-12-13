885cc1eb511a6f446d6742fa7aca2336f11f9ea1
Tyler <tyler.c.smith04@gmail.com>
2023-12-12 21:43:13 -0700

    Update wins and losses chart

--- astros.py
+++ astros.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Function to create an interactive plot for a given season with custom bar colors
 def create_season_plot(astros, season):
@@ -49,8 +50,8 @@ def main():
     st.title("Houston Astros Divisional Leads and Deficits")
     st.write('Select a season to explore how many games the Astros have led or trailed in their division by season! Hover over the chart to see how many games the Astros were leading or trailing their division at any given moment during the season.')
 
-    # Load your data here
     astros = pd.read_csv('astros.csv')
+    astros['W/L'] = astros['W/L'].str.replace('*', '', regex=False)
 
     seasons = range(2017, 2024)
     selected_season = st.selectbox("Select a Season", seasons)
@@ -61,11 +62,6 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
 # Function to create an interactive histogram for a given season and run type
 def create_runs_histogram(astros, season, run_type):
     astros_season = astros[astros['Season'] == season]
@@ -74,7 +70,7 @@ def create_runs_histogram(astros, season, run_type):
     column = 'R' if run_type == 'Runs Scored' else 'RA'
     color = '#002D62' if run_type == 'Runs Scored' else '#EB6E1F'
 
-    # Create the histogram using Plotly
+    # Create histogram using Plotly
     fig = px.histogram(
         astros_season, 
         x=column, 
@@ -102,13 +98,15 @@ def create_runs_histogram(astros, season, run_type):
 
     return fig
 
-# Streamlit app
+# Astros Run Distributions
+
 def main():
     st.title("Astros Runs Distribution")
     st.write('Select a season and whether you want to view the distribution for runs scored by the Astros or runs allowed by the Astros.')
 
     # Load your data here
     astros = pd.read_csv('astros.csv')
+    astros['W/L'] = astros['W/L'].str.replace('*', '', regex=False)
 
     seasons = astros['Season'].unique()
     selected_season = st.selectbox("Select a Season", seasons)
@@ -124,14 +122,14 @@ if __name__ == "__main__":
     main()
 
 
-# Streamlit app
+# Record Against Opponents
 
 def main():
     st.title("Record Against Opponents")
     st.write("The Astros' success between 2017 and 2023 has come at the expense of many around MLB. Search for your favorite team to see how they have done against the Astros!")
 
-    # Load your data here if it's not already loaded
     astros = pd.read_csv('astros.csv')
+    astros['W/L'] = astros['W/L'].str.replace('*', '', regex=False)
 
     # Dropdown for selecting the opponent, sorted alphabetically
     opponents = sorted(astros['Opp'].unique())
@@ -145,6 +143,8 @@ def main():
     win_loss_counts['Win_Percentage'] = win_loss_counts['W'] / (win_loss_counts['W'] + win_loss_counts['L'])
 
     # Display the record against the selected opponent
+    # Make sure to account for errors if no data is available for certain opponents or year
+
     if selected_opponent in win_loss_counts.index:
         st.write(f"Record against {selected_opponent} between 2017-2023:")
         st.write(win_loss_counts.loc[selected_opponent])
@@ -153,11 +153,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 # Function to plot the overall wins and losses over time
 def plot_overall_wins_losses(astros):
@@ -180,11 +175,6 @@ def plot_overall_wins_losses(astros):
     plt.grid(True)
     return plt
 
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
 # Function to plot the overall wins and losses over time
 def plot_overall_wins_losses(astros_season_record_filtered):
     plt.figure(figsize=(10, 6))
@@ -204,13 +194,14 @@ def plot_overall_wins_losses(astros_season_record_filtered):
     plt.grid(True)
     return plt
 
-# Streamlit app
+# Regular Season Success
+
 def main():
     st.title("Astros Regular Season Success")
     st.write("The Astros have won more than 90 games each season between 2017 and 2023, with the exception of a shortened season in 2020 where each team only played 60 games. For each season that you select, you will see the Astros' record along with their longest winning and losing streaks for that season.")
 
-    # Load your data here if it's not already loaded
     astros = pd.read_csv('astros.csv')
+    astros['W/L'] = astros['W/L'].str.replace('*', '', regex=False)
 
     # Calculate the length of the streaks
     astros['Win Streak'] = astros['Streak'].apply(lambda x: x.count('+'))
@@ -236,7 +227,6 @@ def main():
     astros_season_record_filtered = astros_season_record[astros_season_record.index != 2020]
 
     # Plot the overall wins and losses chart
-    # st.write("Houston Astros Wins and Losses by Season")
     plt = plot_overall_wins_losses(astros_season_record_filtered.reset_index())
     st.pyplot(plt)
 
@@ -303,10 +293,24 @@ def plot_win_percentage_by_division(division_group_sorted):
     plt.tight_layout()
     return plt
 
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+# Function to plot win percentage by division
+def plot_win_percentage_by_division(division_group_sorted):
+    plt.figure(figsize=(12, 8))
+    barplot = sns.barplot(x='Win_Percentage', y='Division', data=division_group_sorted, palette=['#002D62', '#EB6E1F'])
+
+    plt.title('Win Percentage by Division')
+    plt.xlabel('Win Percentage')
+    plt.ylabel('Division')
+
+    # Annotate each bar with the value of the win percentage
+    for p in barplot.patches:
+        width = p.get_width()
+        plt.text(width if width > 0.01 else 0.01,
+                 p.get_y() + p.get_height() / 2,
+                 f'{width:.3f}',
+                 ha='left', va='center')
+    plt.tight_layout()
+    return plt
 
 # Function to plot win percentage by division
 def plot_win_percentage_by_division(division_group_sorted):
@@ -327,10 +331,24 @@ def plot_win_percentage_by_division(division_group_sorted):
     plt.tight_layout()
     return plt
 
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+# Function to plot win percentage by division
+def plot_win_percentage_by_division(division_group_sorted):
+    plt.figure(figsize=(12, 8))
+    barplot = sns.barplot(x='Win_Percentage', y='Division', data=division_group_sorted, palette=['#002D62', '#EB6E1F'])
+
+    plt.title('Win Percentage by Division')
+    plt.xlabel('Win Percentage')
+    plt.ylabel('Division')
+
+    # Annotate each bar with the value of the win percentage
+    for p in barplot.patches:
+        width = p.get_width()
+        plt.text(width if width > 0.01 else 0.01,
+                 p.get_y() + p.get_height() / 2,
+                 f'{width:.3f}',
+                 ha='left', va='center')
+    plt.tight_layout()
+    return plt
 
 # Function to plot win percentage by division
 def plot_win_percentage_by_division(division_group_sorted):
@@ -351,58 +369,7 @@ def plot_win_percentage_by_division(division_group_sorted):
     plt.tight_layout()
     return plt
 
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-# Function to plot win percentage by division
-def plot_win_percentage_by_division(division_group_sorted):
-    plt.figure(figsize=(12, 8))
-    barplot = sns.barplot(x='Win_Percentage', y='Division', data=division_group_sorted, palette=['#002D62', '#EB6E1F'])
-
-    plt.title('Win Percentage by Division')
-    plt.xlabel('Win Percentage')
-    plt.ylabel('Division')
-
-    # Annotate each bar with the value of the win percentage
-    for p in barplot.patches:
-        width = p.get_width()
-        plt.text(width if width > 0.01 else 0.01,
-                 p.get_y() + p.get_height() / 2,
-                 f'{width:.3f}',
-                 ha='left', va='center')
-    plt.tight_layout()
-    return plt
-
-import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-# Function to plot win percentage by division
-def plot_win_percentage_by_division(division_group_sorted):
-    plt.figure(figsize=(12, 8))
-    barplot = sns.barplot(x='Win_Percentage', y='Division', data=division_group_sorted, palette=['#002D62', '#EB6E1F'])
-
-    plt.title('Win Percentage by Division')
-    plt.xlabel('Win Percentage')
-    plt.ylabel('Division')
-
-    # Annotate each bar with the value of the win percentage
-    for p in barplot.patches:
-        width = p.get_width()
-        plt.text(width if width > 0.01 else 0.01,
-                 p.get_y() + p.get_height() / 2,
-                 f'{width:.3f}',
-                 ha='left', va='center')
-    plt.tight_layout()
-    return plt
-
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
+# Account for errors that could arise if there are no W's or L's
 def ensure_w_l_columns(df):
     if 'W' not in df.columns:
         df['W'] = 0
@@ -434,11 +401,12 @@ def main():
     st.title('Astros Success Against Each Division')
     st.write("Explore the Astros' success against the different MLB divisions. The Astros face the teams in the American League every year and only a select number of teams from the National League each season. For that reason, sample sizes against the National League divisions will be smaller.")
     astros = pd.read_csv('astros.csv')
+    astros['W/L'] = astros['W/L'].str.replace('*', '', regex=False)
 
     season = st.selectbox('Select Season', sorted(astros['Season'].unique()))
     division = st.selectbox('Select Division', ['AL East', 'AL Central', 'AL West', 'NL East', 'NL Central', 'NL West'])
 
-    # Ensure this mapping is correct and matches the 'Opp' column values in your CSV
+    # Dictionary for the divisions and teams
     divisions_to_teams = {
         'AL East': ['NYY', 'BAL', 'TOR', 'BOS', 'TB'],
         'AL Central': ['CLE', 'DET', 'CHW', 'KCR', 'MIN'],
